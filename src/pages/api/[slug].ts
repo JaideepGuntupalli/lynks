1220073b9f382a878f509ca16d63a71cdb4deea8
Jaideep <guntupallijaideep@gmail.com>
2022-11-07 11:01:26 +0530

    Feat: Kinda added shortlinks

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/db/prisma";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const slug = req.query["slug"];
+
+    if (!slug || typeof slug !== "string") {
+        res.status(400).json({ error: "Missing slug" });
+        return;
+    }
+
+    const data = await prisma.shortLink.findFirst({
+        where: {
+            slug: {
+                equals: slug,
+            },
+        },
+    });
+
+    if (!data) {
+        res.status(404).json({ error: "Not found" });
+        return;
+    }
+
+    return res.redirect(data.url);
+};
